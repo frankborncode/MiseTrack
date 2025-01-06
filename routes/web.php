b3224999f8f33c759680e15ac38f84512705578a
Frank Born <githubfrankborn@outlook.com>
2025-01-06 14:36:44 +0100

    Clean-up blade layout and deleted unnescessary blade files

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/prep', function () {
-    return view('prep');
-});
-
-Route::get('/recipes', function () {
-    return view('recipes');
-});
-
-Route::get('/calendar', function () {
-    return view('calendar');
-});
