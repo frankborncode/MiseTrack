9b38135138cb84291a12aaecfeec3a08f6c125a9
Frank Born <githubfrankborn@outlook.com>
2025-01-09 14:10:19 +0100

    Update(Changend title to MiseTrack)

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>edfef</title>
+    <title>MiseTrack</title>
     {{-- Load TailwindCSS and other Vite-managed assets --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
